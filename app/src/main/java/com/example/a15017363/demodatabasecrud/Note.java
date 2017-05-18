bf061bc6bbd88ac294464d70c946e3a6f14d7a02
15017363 <15017363@myrp.edu.sg>
2017-05-18 15:01:56 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a15017363/demodatabasecrud/Note.java
@@ -0,0 +1,36 @@
+package com.example.a15017363.demodatabasecrud;
+
+import java.io.Serializable;
+
+/**
+ * Created by 15017363 on 18/5/2017.
+ */
+
+public class Note implements Serializable {
+    private int id;
+    private String noteContent;
+
+    public Note(int id, String noteContent) {
+        this.id = id;
+        this.noteContent = noteContent;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNoteContent() {
+        return noteContent;
+    }
+
+    public void setNoteContent(String noteContent) {
+        this.noteContent = noteContent;
+    }
+    @Override
+    public String toString() {
+        return "ID:" + id + ", " + noteContent;
+    }
+
+
+
+}
